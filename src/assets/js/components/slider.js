064286ea649cdb6c7a82b644eb2b6c56485b7fc6
Kraiovyi Denys <tonopik@gmail.com>
2023-04-30 23:33:34 +0300

    start adaptation for sections

--- src/assets/js/components/slider.js
+++ src/assets/js/components/slider.js
@@ -12,6 +12,23 @@ export const swiper = () => {
             rotate: 40,
             slideShadows: true,
         },
+        breakpoints: {
+          320: {
+            slidesPerView: 1,
+          },
+          820: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+            coverflowEffect: {
+              rotate: 20,
+              slideShadows: true,
+          },
+          },
+          1280: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+          }
+        },
         autoplay: {
             delay: 4000,
             pauseOnMouseEnter: true
